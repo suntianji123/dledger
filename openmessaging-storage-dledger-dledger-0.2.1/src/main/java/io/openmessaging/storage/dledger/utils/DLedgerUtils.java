1e9fc314838da1d8f56a01e76f86d5b64bb143a0
suntianji <549733826@qq.com>
2021-01-26 14:52:17 +0800

    第一次提交

---
+++ openmessaging-storage-dledger-dledger-0.2.1/src/main/java/io/openmessaging/storage/dledger/utils/DLedgerUtils.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openmessaging.storage.dledger.utils;
+
+import java.io.File;
+import java.text.NumberFormat;
+import java.util.Calendar;
+
+public class DLedgerUtils {
+    public static void sleep(long sleepMs) {
+        try {
+            Thread.sleep(sleepMs);
+        } catch (Throwable ignored) {
+
+        }
+    }
+
+    public static long elapsed(long start) {
+        return System.currentTimeMillis() - start;
+    }
+
+    public static String offset2FileName(final long offset) {
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMinimumIntegerDigits(20);
+        nf.setMaximumFractionDigits(0);
+        nf.setGroupingUsed(false);
+        return nf.format(offset);
+    }
+
+    public static long computeEclipseTimeMilliseconds(final long beginTime) {
+        return System.currentTimeMillis() - beginTime;
+    }
+
+    public static boolean isItTimeToDo(final String when) {
+        String[] whiles = when.split(";");
+        if (whiles.length > 0) {
+            Calendar now = Calendar.getInstance();
+            for (String w : whiles) {
+                int nowHour = Integer.parseInt(w);
+                if (nowHour == now.get(Calendar.HOUR_OF_DAY)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static double getDiskPartitionSpaceUsedPercent(final String path) {
+        if (null == path || path.isEmpty())
+            return -1;
+
+        try {
+            File file = new File(path);
+
+            if (!file.exists())
+                return -1;
+
+            long totalSpace = file.getTotalSpace();
+
+            if (totalSpace > 0) {
+                long freeSpace = file.getFreeSpace();
+                long usedSpace = totalSpace - freeSpace;
+
+                return usedSpace / (double) totalSpace;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+        return -1;
+    }
+}
